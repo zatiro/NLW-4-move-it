5e66088f1ba6bd2a3f6bac27002ca3d3a5676728
Tiago Cardoso <40040933+zatiro@users.noreply.github.com>
2021-02-24 20:17:45 -0300

    Source code from class #1 and #2

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/global.css'
+
+// Esse arquivo é recalculado toda vez quando o usuário mudar de página
+// Ele contém todos os componentes que estão presentes em toda a aplicação
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
